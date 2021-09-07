b733ed8a9ad4553f83c5a4b4d35d23ae40006d49
jerismith32 <jerismith32@gmail.com>
2021-09-07 10:16:10 -0400

    updated download capability with resume

--- src/Pages/Resume/resume.js
+++ src/Pages/Resume/resume.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+// import Resume from '../../assets/pdf/Resume.pdf';
 
 export default function Resume() {
     return (
         <div id="resume">
-            <h1>On the Resume!</h1>
-            {/* List of Skills
-            Link to Download the PDF */}
+            
+            <h1> Development Skills </h1>
+            <ul>
+                <li> HTML </li>
+                <li> CSS </li>
+                <li>JavaScript</li>
+                <li>CSS Templates and Frameworks such as Bootstrap</li>
+                <li>Web APIs</li>
+                <li>JQuery</li>
+                <li>Node.js</li>
+                <li>Testing with Jest</li>
+                <li>Express.js</li>
+                <li>SQL</li>
+                <li>Handlebars.js</li>
+                <li>MongoDB</li>
+                <li>Progressive Web Applications</li>
+                <li>React</li>
+            </ul>
+            <h4>
+                <a href={ require("../../assets/pdf/Resume.pdf").default} download>Click Here to Download My Resume!</a>
+            </h4>
         </div>
     )
 }
